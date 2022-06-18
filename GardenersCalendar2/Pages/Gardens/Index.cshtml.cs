faaa3ac9acea565c192583fef0c61c2eebb13993
Gavin Selzer <gbselzer@gmail.com>
2022-06-18 13:40:11 -0500

    copied the Calendar into Garden

--- GardenersCalendar2/Pages/Gardens/Index.cshtml.cs
+++ GardenersCalendar2/Pages/Gardens/Index.cshtml.cs
@@ -9,6 +9,7 @@ using GardenersCalendar2.Data;
 using GardenersCalendar2.Data.EFClasses;
 using Microsoft.AspNetCore.Identity;
 using GardenersCalendar2.Data.GardenerUserNS;
+using GardenersCalendar2.Services;
 
 namespace GardenersCalendar2.Pages.Gardens
 {
@@ -16,13 +17,17 @@ namespace GardenersCalendar2.Pages.Gardens
     {
         private readonly GardenersCalendar2.Data.ApplicationDbContext _context;
         private readonly UserManager<GardenerUserClass> _userManager;
-        public IndexModel(GardenersCalendar2.Data.ApplicationDbContext context, UserManager<GardenerUserClass> userManager)
+        private readonly FullCalendarService _calendar;
+        public IndexModel(GardenersCalendar2.Data.ApplicationDbContext context, UserManager<GardenerUserClass> userManager, FullCalendarService calendar)
         {
             _context = context;
             _userManager = userManager;
+            _calendar = calendar;
         }
 
         public IList<Garden> Garden { get;set; } = default!;
+        public List<ToDo> ToDos { get; private set; }
+        public string JsonForCalendarEvents { get; private set; }
 
         public async Task OnGetAsync()
         {
@@ -31,6 +36,11 @@ namespace GardenersCalendar2.Pages.Gardens
                 string LoggedInUserId = _userManager.GetUserId(User);
                 Garden = await _context.Gardens.Where(g => g.GardenerUserId == LoggedInUserId).ToListAsync();
             }
+
+            List<ToDo> toDos = _context.ToDos.ToList();
+            ToDos = toDos;
+            JsonForCalendarEvents = _calendar.ConvertListOfToDosToJson(toDos);
+
         }
     }
 }
